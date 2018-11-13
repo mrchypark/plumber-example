fac3e6b032aa85bf9f2ab1b49274ac557f7cc225
Chan-Yub Park <mrchypark@gmail.com>
2018-11-14 08:16:06 +0900

    Update v1.R

--- R/v1.R
+++ R/v1.R
@@ -1,12 +1,3 @@
-#
-# This is a Plumber API. You can run the API by clicking
-# the 'Run API' button above.
-#
-# Find out more about building APIs with Plumber here:
-#
-#    https://www.rplumber.io/
-#
-
 library(plumber)
 
 #* @apiTitle Plumber Example API
